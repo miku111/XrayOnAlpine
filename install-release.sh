1b8e2fcfe0548643625241d6a3077f903bd18393
Steven Ramsey <84562264+miku111@users.noreply.github.com>
2025-11-28 21:23:20 +0800

    Update install-release.sh

--- install-release.sh
+++ install-release.sh
@@ -29,6 +29,7 @@ fi
 shortId1=$(getShortId)
 shortId2=$(getShortId)
 sniName='uedata.amazon.com'
+export sniName
     # Step 4: Create the Xray configuration file
 cat >/usr/local/etc/xray/config.json<<EOF
 {
